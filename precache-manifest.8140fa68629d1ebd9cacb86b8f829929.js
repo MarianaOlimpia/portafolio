0c5722ba9a09c4afb48113cbbe48b872e01c6434
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-07-26 19:28:29 -0500

    Updates

---
+++ precache-manifest.8140fa68629d1ebd9cacb86b8f829929.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8d5720743bc77b245bb9b6c550ddde",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "543f42a9854704b9a835",
+    "url": "/portafolio/static/css/main.3571bb04.chunk.css"
+  },
+  {
+    "revision": "de5a70f8741840abb80d",
+    "url": "/portafolio/static/js/2.41701ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.41701ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543f42a9854704b9a835",
+    "url": "/portafolio/static/js/main.de6a70e4.chunk.js"
+  },
+  {
+    "revision": "ca7106a6a095f1d16e5d",
+    "url": "/portafolio/static/js/runtime-main.febf8ef0.js"
+  },
+  {
+    "revision": "a307a626a673c910d64d1bb2e84d0e5b",
+    "url": "/portafolio/static/media/cipher.a307a626.jpg"
+  },
+  {
+    "revision": "184959a42737a75b466792ec4a44d88d",
+    "url": "/portafolio/static/media/email.184959a4.png"
+  },
+  {
+    "revision": "ff3af7182411362a5b1f459beadfb9cf",
+    "url": "/portafolio/static/media/github.ff3af718.png"
+  },
+  {
+    "revision": "e329191b743276ae9100ea12589daab3",
+    "url": "/portafolio/static/media/react.e329191b.png"
+  },
+  {
+    "revision": "e365efb8f65d335b61874fafe78aa6fd",
+    "url": "/portafolio/static/media/social-network.e365efb8.png"
+  },
+  {
+    "revision": "e48dee218bd807c615d9f057691eb812",
+    "url": "/portafolio/static/media/steam.e48dee21.png"
+  }
+]);
