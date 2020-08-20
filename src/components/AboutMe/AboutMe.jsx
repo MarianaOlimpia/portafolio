0c6aae4418c95876c90c1d6e285dd314320a091d
MarianaOlimpia <mariana.olimpiap@gmail.com>
2020-08-19 23:59:01 -0500

    the projects section is modified, skill names are added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,6 +7,9 @@ import jsIcon from '../../Assets/javascript.png';
 import firebaseIcon from '../../Assets/firebase.png';
 import reactIcon from '../../Assets/react.png';
 import bootstrapIcon from '../../Assets/bootstrap.png';
+import materialui from '../../Assets/material-ui.png';
+import antdesign from '../../Assets/ant-design.png';
+
 
 const AboutMe = () => {
     return ( 
@@ -21,32 +24,42 @@ const AboutMe = () => {
                         <div className="img iconContainer"><img src={htmlIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >HTML5</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>HTML 5</p></div>
                     <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
                         <div className="img iconContainer"><img src={cssIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >CSS3</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>CSS 3</p></div>
                     <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
                         <div className="img iconContainer"><img src={jsIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >Javascript</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>Javascript</p></div>
                     <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
                         <div className="img iconContainer"><img src={firebaseIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >Firebase</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>Firebase</p></div>
                     <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
                         <div className="img iconContainer"><img src={reactIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >React</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>React JS</p></div>
                     <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
                         <div className="img iconContainer"><img src={bootstrapIcon} alt="img" /></div>
                         <div className="info">
                         <h3 >Bootstrap</h3>
-                    </div></div></div>
+                    </div></div><p className='title'>Bootstrap</p></div>
+                    <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
+                        <div className="img iconContainer"><img src={antdesign} alt="img" /></div>
+                        <div className="info">
+                        <h3 >Ant Design</h3>
+                    </div></div><p className='title'>Ant Design</p></div>
+                    <div className="ih-item circle colored effect15 left_to_right"><div className='hoverdiv'>
+                        <div className="img iconContainer"><img src={materialui} alt="img" /></div>
+                        <div className="info">
+                        <h3 >Material UI</h3>
+                    </div></div><p className='title'>Material UI</p></div>
                 </div>
             </div>
             </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Projects.scss';
-import cipher from '../../Assets/cipher.jpg';
-import steam from '../../Assets/steam.png';
-import redsocial from '../../Assets/social-network.png';
+import datalovers from '../../Assets/datalovers.PNG';
+import socialnetwork from '../../Assets/socialnetwork.PNG';
+import burgerqueen from '../../Assets/burgerqueen.PNG';
+import reactIcon from '../../Assets/icon-react.png';
+import materialIcon from '../../Assets/icon-materialui.png';
+import antdesignIcon from '../../Assets/icon-antdesign.png';
+import firebaseIcon from '../../Assets/icon-firebase.png';
+import htmlIcon from '../../Assets/icon-html5.png';
+import cssIcon from '../../Assets/icon-css3.png';
+import jsIcon from '../../Assets/icon-js.png';
 
 const Projects = () => {
     return ( 
@@ -11,18 +18,23 @@ const Projects = () => {
 
             <div className='row cards-container'>
                 <div className="card card-project m-3 mx-auto">
-                    <img src={cipher} className="card-img-top" alt="..." />
+                    <img src={datalovers} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Cipher</h5>
-                        <p className="card-text">Sirve para que el usuario pueda cifrar y descifrar un texto indicando un desplazamiento específico de caracteres (offset)</p>
+                        <h5 className="card-title">Data Lovers</h5>
+                        <p className="card-text">Interfaz web responsiva donde se puede consultar y manipular data por medio de filtros.</p>
+                        <div>
+                            <h6>1 semana</h6>
+                            <img src={reactIcon} className="projects-icons" alt="..." />
+                            <img src={materialIcon} className="projects-icons" alt="..." />
+                        </div>
                         <a 
-                            href="https://github.com/MarianaOlimpia/CDMX009-cipher" 
+                            href="https://github.com/MarianaOlimpia/data-lovers-react" 
                             target='_blank' 
                             rel="noopener noreferrer" 
                             className="btn btn-primary"
                         >Repositorio</a>
                         <a 
-                            href="https://github.com/MarianaOlimpia/CDMX009-cipher" 
+                            href="https://marianaolimpia.github.io/data-lovers-react/" 
                             target='_blank' 
                             rel="noopener noreferrer" 
                             className="btn btn-primary">Demo</a>
@@ -30,17 +42,23 @@ const Projects = () => {
                 </div>
 
                 <div className="card card-project m-3 mx-auto">
-                    <img src={steam} className="card-img-top" alt="..." />
+                    <img src={burgerqueen} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Steam Noticias</h5>
-                        <p className="card-text">Steam noticias: Lista noticias relacionadas a los videojuegos presentes en la plataforma de Steam.</p>
+                        <h5 className="card-title">Burger Queen</h5>
+                        <p className="card-text">Interfaz en la que puedan tomar pedidos usando una tablet (iPad), y enviarlos a la cocina para que se preparen ordenada y eficientemente.</p>
+                        <div>
+                            <h6>7 semanas</h6>
+                            <img src={reactIcon} className="projects-icons" alt="..." />
+                            <img src={antdesignIcon} className="projects-icons" alt="..." />
+                            <img src={firebaseIcon} className="projects-icons" alt="..." />
+                        </div>
                         <a 
-                            href="https://github.com/MarianaOlimpia/CDMX009-Data-Lovers" 
+                            href="https://github.com/MarianaOlimpia/CDMX009-BurgerQueen" 
                             target='_blank' 
                             rel="noopener noreferrer"
                             className="btn btn-primary">Repositorio</a>
                         <a 
-                            href="https://marianaolimpia.github.io/CDMX009-Data-Lovers/" 
+                            href="https://burgerqueen-d2dca.web.app/" 
                             target='_blank' 
                             rel="noopener noreferrer"
                             className="btn btn-primary">Demo</a>
@@ -48,10 +66,17 @@ const Projects = () => {
                 </div>
 
                 <div className="card card-project m-3 mx-auto">
-                    <img src={redsocial} className="card-img-top" alt="..." />
+                    <img src={socialnetwork} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Code Makers</h5>
-                        <p className="card-text">El objetivo de este proyecto es construir una Red Social, Single-Page Application (SPA), responsiva en la que podamos escribir, leer, actualizar y eliminar datos.</p>
+                        <p className="card-text">Red Social, Single-Page Application (SPA), responsiva en la que podemos escribir, leer, actualizar y eliminar datos.</p>
+                        <div>
+                            <h6>4 semanas</h6>
+                            <img src={htmlIcon} className="projects-icons" alt="..." />
+                            <img src={cssIcon} className="projects-icons" alt="..." />
+                            <img src={jsIcon} className="projects-icons" alt="..." />
+                            <img src={firebaseIcon} className="projects-icons" alt="..." />
+                        </div>
                         <a 
                             href="https://github.com/lauraac/CDMX009-Social-Network" 
                             target='_blank' 
